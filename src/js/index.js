e3ebd69b04d8ddec1acbe9c280a35593c98f0ce4
Edson Manuel Carballo Vera <edsonmanuelcarballovera@gmail.com>
2021-03-12 14:39:48 -0600

    Recomendaciones de CodeFactor

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 if ("serviceWorker" in navigator) {
 	navigator.serviceWorker.register("sw.js").then(e => {
 		console.log("Registrado");
+		console.log(e);
 	}).catch(error => {
 		console.error(error)
 	});
