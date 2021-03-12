6a2fda44c7dd37e62672b2e68fc2225e6c4d066a
Edson Manuel Carballo Vera <edsonmanuelcarballovera@gmail.com>
2021-03-12 08:55:37 -0600

    Recomendaciones de Codacy

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,3 @@
 if ("serviceWorker" in navigator) {
-	navigator.serviceWorker.register("sw.js").then((registration) => {
-		console.error("SW Registered");
-		console.error(registration);
-	}).catch((error) => {
-		console.error("SW Registration Failed");
-		console.error(error);
-	});
+	navigator.serviceWorker.register("sw.js").then().catch();
 }
