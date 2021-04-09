c28c6a05be07432e8e64d5c7b320055c30272bd9
Edson Manuel Carballo Vera <edsonmanuelcarballovera@gmail.com>
2021-04-09 15:59:20 -0500

    Prueba de SweetAlert en login

--- src/js/login.js
+++ src/js/login.js
@@ -2,3 +2,19 @@ $("#registerButton").click(function () {
 	loadContent("src/view/register.html");
 	savePage();
 });
+
+$("#loginButton").click(function () {
+	if ($("#emailInput").val() === "" ||
+		$("#passwordInput").val() === "") {
+		Swal.fire({
+			icon: 'error',
+			title: 'Campos vacíos',
+			text: 'Uno o más campos se encuentran vacíos'
+		});
+	} else {
+		Swal.fire({
+			icon: 'success',
+			title: 'Correcto!'
+		})
+	}
+});
