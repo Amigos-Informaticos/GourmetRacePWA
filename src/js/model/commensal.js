d68a649771e6d8804a309caf40a0930d94c8a523
Edson Manuel Carballo Vera <edsonmanuelcarballovera@gmail.com>
2021-04-16 19:41:00 -0500

    Implementación de bloque try para declarar clase Commensal Se ha incluido un bloque try para evitar que la consola muestre la excepción cuando se vuelve a importar un archivo que declara una clase

--- src/js/model/commensal.js
+++ src/js/model/commensal.js
@@ -1,42 +1,42 @@
-﻿class Commensal {
-	email;
-	password;
+﻿try {
+	class Commensal {
+		email;
+		password;
 
-	constructor(email, password) {
-		this.email = email;
-		this.password = password;
-	}
-
-	get email() {
-		return this.email;
-	}
-
-	get password() {
-		return this.password;
-	}
-
-	set email(email) {
-		if (this.isEmail(email)) {
+		constructor(email, password) {
 			this.email = email;
-		}
-	}
-
-	set password(password) {
-		if (this.isPassword(password)) {
 			this.password = password;
 		}
+
+		get email() {
+			return this.email;
+		}
+
+		get password() {
+			return this.password;
+		}
+
+		set email(email) {
+			if (this.isEmail(email)) {
+				this.email = email;
+			}
+		}
+
+		set password(password) {
+			if (this.isPassword(password)) {
+				this.password = password;
+			}
+		}
+
+		isEmail(email) {
+			return email.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/) != null;
+		}
+
+		isPassword(password) {
+			return password.match(/^(?=.*?[0-z]).{8,}$/) != null;
+		}
 	}
 
-	isEmail(email) {
-		return email.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/) != null;
-	}
-
-	isPassword(password) {
-		return password.match(/^(?=.*?[0-z]).{8,}$/) != null;
-	}
-}
-
-try {
 	module.exports = Commensal;
 } catch (referenceError) {
 
