f8f115eb0990c30f6f8c8d18d72d1ba3c0b952a5
Edson Manuel Carballo Vera <edsonmanuelcarballovera@gmail.com>
2021-04-16 19:25:36 -0500

    Formateo de archivo commensal.js

--- src/js/model/commensal.js
+++ src/js/model/commensal.js
@@ -1,41 +1,43 @@
 ﻿class Commensal {
-    email;
-    password;
-    
-    constructor(email, password) {
-        this.email = email;
-        this.password = password;
-    }
+	email;
+	password;
 
-    get email() {
-        return this.email;
-    }
+	constructor(email, password) {
+		this.email = email;
+		this.password = password;
+	}
 
-    get password() {
-        return this.password;
-    }
-    
-    set email(email){
-        if (this.isEmail(email)){
-            this.email = email;
-        }
-    }
-    
-    set password(password){
-        if (this.isPassword(password)){
-            this.password = password;
-        }
-    }
+	get email() {
+		return this.email;
+	}
 
-    isEmail(email){
-        return email.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/) != null;
-    }
-    isPassword(password){
-        return password.match(/^(?=.*?[0-z]).{8,}$/) != null;
-    }
+	get password() {
+		return this.password;
+	}
+
+	set email(email) {
+		if (this.isEmail(email)) {
+			this.email = email;
+		}
+	}
+
+	set password(password) {
+		if (this.isPassword(password)) {
+			this.password = password;
+		}
+	}
+
+	isEmail(email) {
+		return email.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/) != null;
+	}
+
+	isPassword(password) {
+		return password.match(/^(?=.*?[0-z]).{8,}$/) != null;
+	}
 }
-try{
-    module.exports = Commensal;    
-}catch (referenceError){
-    
+
+try {
+	module.exports = Commensal;
+} catch (referenceError) {
+
 }
