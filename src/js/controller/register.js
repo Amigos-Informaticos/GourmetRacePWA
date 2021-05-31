76595970f38565d192062aba1e190f5bc3f17cbb
Alexis Alvarez <alexisao@hotmail.com>
2021-05-31 09:30:51 -0500

    Interfaz SignUp para comensal
    *html
    *JS

--- src/js/controller/register.js
+++ src/js/controller/register.js
@@ -1,29 +1,54 @@
 $("#cancelButton").click(function () {
-	loadContent("src/view/login.html");
+	window.location = "/";
 });
 
-$("#registerButton2").click(function () {
-
-	console.log("click");
+document.querySelector("#registerForm").onsubmit = function (){	
+	let email = $("#emailInput").val(),
+	password = $("#passwordInput").val(),
+	username = $("#usernameInput").val();
+	
 	if (validateRegister()) {
-		Swal.fire({
-			icon: 'success',
-			title: 'Correcto'
+		register(email, password, username).then(success => {			
+			if(success.status === 201) {
+				Swal.fire({
+					icon: 'success',
+					title: 'Comensal registrado con exito'					
+				});				
+			} else if (success.status === 409) {
+				Swal.fire({
+					icon: 'warning',
+					title: 'El comensal ya ha sido registrado previamente'				
+				});
+			}
+		}).catch(failure => {
+			Swal.fire({
+				icon: 'error',
+				title: 'Fallo al conectar con el servidor'				
+			});
 		});
+		return false;		
 	} else {
 		Swal.fire({
-			icon: 'error',
+			icon: 'warning',
 			title: 'Campos incorrectos',
 			text: 'Uno o más campos son incorrectos'
-		});
+		});		
 	}
-});
+};
 
 function validateRegister() {
+	let email = $("#emailInput").val();
+	let password = $("#passwordInput").val();
+	let username = $("#usernameInput").val();
 	let commensal = new Commensal();
-	let email = $("#emailInput");
-	let password = $("#passwordInput");
-	let username = $("#usernameInput");
 	return commensal.isEmail(email) &&
 		commensal.isPassword(password);
+}
+
+async function register(email, password, username) {
+	const payload = {email, password, username};
+	const connection = new Connection("http://amigosinformaticos.ddns.net:42066");
+	const response = await connection.send("post", "commensals", null, payload);	
+	return response;
+	
 }
