90b44e41c65a83d9918a9d88f68e6e19e29709f0
Edson Manuel Carballo Vera <edsonmanuelcarballovera@gmail.com>
2021-06-21 00:17:26 -0500

    Pruebas de compatibilidad

--- src/js/controller/register.js
+++ src/js/controller/register.js
@@ -1,45 +1,44 @@
 $("#cancelButton").click(function () {
-	window.location = "/";
+	window.location = "../../index.html";
 });
 
-document.querySelector("#registerForm").onsubmit = function (){	
+document.querySelector("#registerForm").onsubmit = function () {
 	let email = $("#emailInput").val(),
-	password = $("#passwordInput").val(),
-	username = $("#usernameInput").val();
-	
+		password = $("#passwordInput").val(),
+		username = $("#usernameInput").val();
+
 	if (validateRegister()) {
-		register(email, password, username).then(success => {			
-			if(success.status === 201) {
+		register(email, password, username).then(success => {
+			if (success.status === 201) {
 				Swal.fire({
 					icon: 'success',
-					title: 'Comensal registrado con exito'					
-				});				
+					title: 'Comensal registrado con exito'
+				});
 			} else if (success.status === 409) {
 				Swal.fire({
 					icon: 'warning',
-					title: 'El comensal ya ha sido registrado previamente'				
+					title: 'El comensal ya ha sido registrado previamente'
 				});
 			}
-		}).catch(failure => {
+		}).catch(() => {
 			Swal.fire({
 				icon: 'error',
-				title: 'Fallo al conectar con el servidor'				
+				title: 'Fallo al conectar con el servidor'
 			});
 		});
-		return false;		
+		return false;
 	} else {
 		Swal.fire({
 			icon: 'warning',
 			title: 'Campos incorrectos',
 			text: 'Uno o más campos son incorrectos'
-		});		
+		});
 	}
 };
 
 function validateRegister() {
 	let email = $("#emailInput").val();
 	let password = $("#passwordInput").val();
-	let username = $("#usernameInput").val();
 	let commensal = new Commensal();
 	return commensal.isEmail(email) &&
 		commensal.isPassword(password);
@@ -48,7 +47,7 @@ function validateRegister() {
 async function register(email, password, username) {
 	const payload = {email, password, username};
 	const connection = new Connection("http://amigosinformaticos.ddns.net:42066");
-	const response = await connection.send("post", "commensals", null, payload);	
+	const response = await connection.send("post", "commensals", null, payload);
 	return response;
-	
+
 }
